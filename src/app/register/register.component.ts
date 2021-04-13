f033ab6a4e7895ed6f7815ba7a3bd7eaf944a519
vivek <vivekd.kulkarni@lntinfotech.com>
2021-04-14 00:19:58 +0530

    Register Form added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,10 +8,27 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
+  user: User=new User();
+
   constructor(private router:Router){};
 
   redirectToDashboard() {
     this.router.navigateByUrl('/dashboard');
   }
+
+}
+
+export class User{
+  name:string;
+  phoneNo:number;
+  email:string;
+  userName:string;
+  address:string;
+  password:string;
+  confirmPassword:string;
+  cardType:string;
+  cardNo:number;
+
+
 
 }
