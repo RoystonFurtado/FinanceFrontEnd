1842112ef777d585e50ee17578f275317bc4c681
vivek <vivekd.kulkarni@lntinfotech.com>
2021-04-20 11:26:04 +0530

    Product description partial front end

---
+++ src/app/product-description/product-description.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { Product } from '../product/product.component';
+
+@Component({
+  selector: 'app-product-description',
+  templateUrl: './product-description.component.html',
+  styleUrls: ['./product-description.component.css']
+})
+export class ProductDescriptionComponent  {
+  product:Product;
+  id:number;
+  constructor( private route: ActivatedRoute,private router:Router){
+    this.route.queryParams.subscribe(params => {
+      this.id = params['id'];
+  });
+    this.product= new Product(this.id,"iphone 11","Apple iphone 11",49900,'assets/product-images/iphone8.jfif');
+  };
+  }
+
+ 
+
+
+
+
