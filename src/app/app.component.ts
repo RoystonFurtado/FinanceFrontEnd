8f4f380d345721c340c1ce5a46f15ae19df55ea0
Kunjal Jain <neeluneelam19@gmail.com>
2021-04-26 12:54:02 +0530

    product listing css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'FinanceFrontEnd';
 }
 
-export var baseUrl = "http://localhost:8223";
+export var baseUrl = "http://localhost:8181";
 export var Entity_UserId = "userId"; 
 export var Entity_emailId = "emailId"; 
 export var Entity_profileStatus = "profileStatus"; 
